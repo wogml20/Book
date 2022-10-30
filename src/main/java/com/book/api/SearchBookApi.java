ea3aad300e0ee58eba16a5acedf67c7f53d0fc8c
김재희 <kimjaehee@gimjaehuis-MacBook-Pro.local>
2022-10-30 23:18:45 +0900

    검색 api 적용(2)

--- src/main/java/com/book/api/SearchBookApi.java
+++ src/main/java/com/book/api/SearchBookApi.java
@@ -1,84 +1,124 @@
-package com.book.api;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-
-public class SearchBookApi {
-
-    public static void main(String[] args) {
-        String clientId = "zwvvcSwJFxCgtvbJTnvm";
-        String clientSecret = "qqkxIC0KAZ";
-        String text = null;
-
-        try {
-            text = URLEncoder.encode("그린팩토리", "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("검섹어 인코딩 실패", e);
-        }
-
-        String apiURL = "https://openapi.naver.com/v1/search/book?query=" + text;
-
-        Map<String, String> requestHeaders = new HashMap<>();
-        requestHeaders.put("X-Naver-Client-Id", clientId);
-        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-        String responseBody = get(apiURL, requestHeaders);
-
-        System.out.println(responseBody);
-    }
-
-    private static String get(String apiUrl, Map<String, String> requestHeaders) {
-        HttpURLConnection con = connect(apiUrl);
-        try {
-            con.setRequestMethod("GET");
-            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
-                con.setRequestProperty(header.getKey(), header.getValue());
-            }
-
-            int responseCode = con.getResponseCode();
-            if(responseCode == HttpURLConnection.HTTP_OK) {
-                return readBody(con.getInputStream());
-            }else {
-                return readBody(con.getErrorStream());
-            }
-        }catch (IOException e) {
-            throw new RuntimeException("API 요청과 응답 실패", e);
-        }finally {
-            con.disconnect();
-        }
-    }
-
-    private static HttpURLConnection connect(String apiUrl) {
-        try {
-            URL url = new URL(apiUrl);
-            return (HttpURLConnection)url.openConnection();
-        }catch (MalformedURLException e) {
-            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
-        }catch (IOException e) {
-            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl,e);
-        }
-    }
-    private static String readBody(InputStream body) {
-        InputStreamReader streamReader = new InputStreamReader(body);
-
-        try(BufferedReader lineReader = new BufferedReader(streamReader)) {
-            StringBuilder responseBody = new StringBuilder();
-
-            String line;
-            while ((line = lineReader.readLine()) != null) {
-                responseBody.append(line);
-            }
-
-            return responseBody.toString();
-        }catch (IOException e) {
-            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
-        }
-    }
-}
+//package com.book.api;
+//
+////import org.json.simple.JSONArray;
+////import org.json.simple.JSONObject;
+////import org.json.simple.parser.JSONParser;
+////import org.json.simple.parser.ParseException;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.http.RequestEntity;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.util.UriComponentsBuilder;
+//
+//import java.io.*;
+//import java.net.*;
+//import java.nio.ByteBuffer;
+//import java.nio.charset.StandardCharsets;
+//import java.text.ParseException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class SearchBookApi {
+//
+//    public static void main(String[] args) {
+//        String clientId = "zwvvcSwJFxCgtvbJTnvm";
+//        String clientSecret = "qqkxIC0KAZ";
+//        String text = null;
+//
+//        try {
+//            text = URLEncoder.encode("그린팩토리", "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException("검섹어 인코딩 실패", e);
+//        }
+//
+//        String apiURL = "https://openapi.naver.com/v1/search/book?query=" + text;
+//
+//        Map<String, String> requestHeaders = new HashMap<>();
+//        requestHeaders.put("X-Naver-Client-Id", clientId);
+//        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+//        String responseBody = get(apiURL, requestHeaders);
+//
+//        System.out.println(responseBody);
+//    }
+//
+//    private static String get(String apiUrl, Map<String, String> requestHeaders) {
+//        HttpURLConnection con = connect(apiUrl);
+//        try {
+//            con.setRequestMethod("GET");
+//            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+//                con.setRequestProperty(header.getKey(), header.getValue());
+//            }
+//
+//            int responseCode = con.getResponseCode();
+//            if(responseCode == HttpURLConnection.HTTP_OK) {
+//                return readBody(con.getInputStream());
+//            }else {
+//                return readBody(con.getErrorStream());
+//            }
+//        }catch (IOException e) {
+//            throw new RuntimeException("API 요청과 응답 실패", e);
+//        }finally {
+//            con.disconnect();
+//        }
+//    }
+//
+//    private static HttpURLConnection connect(String apiUrl) {
+//        try {
+//            URL url = new URL(apiUrl);
+//            return (HttpURLConnection)url.openConnection();
+//        }catch (MalformedURLException e) {
+//            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+//        }catch (IOException e) {
+//            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl,e);
+//        }
+//    }
+//    private static String readBody(InputStream body) {
+//        InputStreamReader streamReader = new InputStreamReader(body);
+//
+//        try(BufferedReader lineReader = new BufferedReader(streamReader)) {
+//            StringBuilder responseBody = new StringBuilder();
+//
+//            String line;
+//            while ((line = lineReader.readLine()) != null) {
+//                responseBody.append(line);
+//            }
+//
+//            return responseBody.toString();
+//        }catch (IOException e) {
+//            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
+//        }
+//    }
+//
+//    public static Object apiTest(String query) throws ParseException {
+//
+//        ByteBuffer buffer = StandardCharsets.UTF_8.encode(query);
+//        String encode = StandardCharsets.UTF_8.decode(buffer).toString();
+//
+//        URI uri = UriComponentsBuilder
+//                .fromUriString("https://openapi.naver.com")
+//                .path("/v1/search/book.json")
+//                .queryParam("query",encode)
+//                .queryParam("display",10)
+//                .encode()
+//                .build()
+//                .toUri();
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        RequestEntity<Void> req = RequestEntity
+//                .get(uri)
+//                .header("X-Naver-Client-Id", "zwvvcSwJFxCgtvbJTnvm")
+//                .header("X-Naver-Client-Secret","qqkxIC0KAZ")
+//                .build();
+//
+//        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+//        String r = result.getBody();
+//
+////        JSONParser parser = new JSONParser();
+////        JSONArray values = (JSONArray)parser.parse(r);
+////        JSONObject value = (JSONObject)values.get(0);
+//
+//        return  value;
+//    }
+//}
--- src/main/java/com/book/controller/Searchcontroller.java
+++ src/main/java/com/book/controller/Searchcontroller.java
@@ -1,33 +1,83 @@
 package com.book.controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
+
+
+import com.mysql.cj.xdevapi.JsonArray;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import lombok.extern.log4j.Log4j2;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+//import org.json.JSONParser;
+//import org.json.ParseException;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+
 
 @RequiredArgsConstructor
 @Controller
+@Log4j2
 @RequestMapping("/book")
 public class Searchcontroller {
 
 //    @RequestParam String query,
+//    @GetMapping("/search")
+//    public String searchBook(@RequestParam String query,Model model) throws ParseException {
+//
+//        ByteBuffer buffer = StandardCharsets.UTF_8.encode(query);
+//        String encode = StandardCharsets.UTF_8.decode(buffer).toString();
+//
+//        URI uri = UriComponentsBuilder
+//                .fromUriString("https://openapi.naver.com")
+//                .path("/v1/search/book.json")
+//                .queryParam("query",encode)
+//                .queryParam("display",10)
+//                .encode()
+//                .build()
+//                .toUri();
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        RequestEntity<Void> req = RequestEntity
+//                .get(uri)
+//                .header("X-Naver-Client-Id", "zwvvcSwJFxCgtvbJTnvm")
+//                .header("X-Naver-Client-Secret","qqkxIC0KAZ")
+//                .build();
+//
+//        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+//        String r = result.getBody();
+//
+//        JSONParser parser = new JSONParser();
+//        JSONArray values = (JSONArray)parser.parse(r);
+//        JSONObject value = (JSONObject)values.get(0);
+//        System.out.println((String)value.get("lastBuildDate"));
+//
+//        model.addAttribute("result", r);
+//        return "api/searchBook";
+//    }
+
     @GetMapping("/search")
-    public String searchBook(@RequestParam String query,Model model) throws ParseException {
+    public String Book() {
+        return "api/searchBook";
+    }
+    @PostMapping("/search")
+    public String searchBook(HttpServletRequest httpServletRequest, Model model) throws ParseException {
+
+        String query = httpServletRequest.getParameter("title");
+        log.info(query);
 
         ByteBuffer buffer = StandardCharsets.UTF_8.encode(query);
         String encode = StandardCharsets.UTF_8.decode(buffer).toString();
@@ -52,12 +102,17 @@ public class Searchcontroller {
         ResponseEntity<String> result = restTemplate.exchange(req, String.class);
         String r = result.getBody();
 
-        JSONParser parser = new JSONParser();
-        JSONArray values = (JSONArray)parser.parse(r);
-        JSONObject value = (JSONObject)values.get(0);
-        System.out.println((String)value.get("lastBuildDate"));
+        JSONObject value = new JSONObject(r);
+        log.info(value);
 
-        model.addAttribute("result", r);
+        JSONArray items = value.getJSONArray("items");
+        log.info(items);
+
+        for(int i = 0; i<items.length(); i++) {
+            log.info(items.getJSONObject(i).get("title"));
+        }
+        model.addAttribute("result", items);
         return "api/searchBook";
     }
+
 }
